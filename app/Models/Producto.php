9aa84137923d7f4a747378b0a6e9c2a1549975bd
Brayan Ortega <brayanortega14245@gmail.com>
2025-12-14 16:55:35 -0500

    Paso 6: Agregar anotaciones de propiedad en modelo Producto para Intelisense

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,6 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $codigo
+ * @property string $nombre
+ * @property float $precio
+ * @property string|null $descripcion
+ * @property string|null $imagen
+ * @property string|null $marca
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class Producto extends Model
 {
     protected $fillable = [
